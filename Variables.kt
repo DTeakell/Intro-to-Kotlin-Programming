268eee8830820ffdee34c15fed3074cd899e36d6
Dillon Teakell <teakelld@icloud.com>
2024-07-18 17:00:57 -0400

    Create Variables.kt

---
+++ Variables.kt
@@ -0,0 +1,13 @@
+// Variables and Constants
+
+fun main() {
+    // To declare a variable (a value that will be changed):
+
+    var age: Int = 22
+
+    // To declare a constant (a value that stays the same - hence constant)
+
+    val name: String = "Dillon"
+
+    println("Hello! I am $name, and I am $age years old!")
+}
